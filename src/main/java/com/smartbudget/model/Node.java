0befc6b79baa76a761037976ab90bc7209df6eb0
Gajindu Gunarathna <gajindugunarathna@gmail.com>
2026-02-22 13:11:04 +0530

    Revert "Merge remote-tracking branch 'origin/master'"
    This reverts commit 3966d10d183dac376c5827b09bff7ce75f56602c, reversing
    changes made to 0fc2f80e0fc93e30f466f468cb2f798c6002a43d.

--- src/main/java/com/smartbudget/model/Node.java
+++ src/main/java/com/smartbudget/model/Node.java
@@ -1,13 +1,4 @@
 package com.smartbudget.model;
 
 public class Node {
-    public Expense data;
-    public Node next;
-
-    public Node(Expense data) {
-        this.data = data;
-        this.next = null;
-    }
-
-
 }
